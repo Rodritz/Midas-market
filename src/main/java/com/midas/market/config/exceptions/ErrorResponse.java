2c2c3c2a14566d8f39f5b9bc0e8fe36d11c425a3
Rodrigo Urquia <rodrigourq@gmail.com>
2023-11-09 02:17:25 -0300

    borrando comentarios

--- src/main/java/com/midas/market/config/exceptions/ErrorResponse.java
+++ src/main/java/com/midas/market/config/exceptions/ErrorResponse.java
@@ -1,19 +1,15 @@
 package com.midas.market.config.exceptions;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
+
     private String message;
     private int status;
 
-
-    public ErrorResponse(String message, int status) {
-        this.message = message;
-        this.status = status;
-    }
-
-    public ErrorResponse() {
-
-    }
 }
